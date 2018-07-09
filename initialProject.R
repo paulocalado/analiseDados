fd8fc32a5d8527ce9702dbdf2f7921587deb5158
Paulo Henrique Calado Aoun <paulo_calado@live.com>
2018-07-09 20:13:51 -0300

    analise projeto

--- initialProject.R
+++ initialProject.R
@@ -1,10 +1,28 @@
 nflData <- read.csv("/Users/paulocalado/Documents/UFRPE/analiseDados/pbp_2017_huge.csv",header = T,stringsAsFactors = F)
-nflData[2,'time']
-eaglesOffense <- subset(nflData, posteam == 'PHI' & DefensiveTeam == 'NYG')
-brownsOffense <- subset(nflData, posteam == 'CLE')
-eaglesDeffense <- subset(nflData, DefensiveTeam == 'PHI')
-steelersOffense <- subset(nflData, posteam == "PIT")
-eaglesNegativePlays<- subset(eaglesOffense, eaglesOffense$Yards.Gained<0)
-barplot(eaglesNegativePlays$PlayType)
-counts<-table(eaglesDeffense$Yards.Gained <3)
-barplot(counts)
+
+eaglesOffenseData <- nflData[nflData$posteam == "PHI",]
+eaglesDefenseData <- nflData[nflData$DefensiveTeam == "WAS",]
+
+brownsOffenseData <- nflData[nflData$posteam == "CLE", ]
+
+View(nflData$posteam == "PHI")
+
+nflData$Yards.Gained
+
+brownsOffenseData$Rusher[brownsOffenseData$PlayType=="Run"]
+table(brownsOffenseData$Rusher[brownsOffenseData$PlayType=="Run"])
+
+aggregate(formula = Yards.Gained ~ RunLocation,
+          FUN = mean,
+          data = eaglesOffenseData)
+
+library(dplyr)
+
+eaglesOffenseData %>%
+  group_by(Rusher) %>%
+  filter(PlayType=="Run") %>%
+  summarise(
+    meanYardsGained = mean(Yards.Gained)
+  )
+
+sum(eaglesOffenseData$PlayType=="Run"&eaglesOffenseData$Rusher=="N.Sudfeld")
